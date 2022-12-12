20654c5c99acb5c1c18556bcc8cde65a15a70c19
asmaaax <94681513+asmaaax@users.noreply.github.com>
2022-12-12 20:23:49 +0000

    new commit

--- App.js
+++ App.js
@@ -1,24 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
 import SignInScreen from './src/screens/SignInScreen';
 import SignUpScreen from './src/screens/SignUpScreen.JS';
+import ChatScreen from './src/screens/ChatScreen';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-import ProfileScreen from './src/screens/ProfileScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   const Stack = createNativeStackNavigator()
   return (
   <NavigationContainer>
-    <Stack.Navigator initialRouteName='Profile'>
-      <Stack.Screen name='Sign In' component={SignInScreen}/>
-      <Stack.Screen name='Sign Up' component={SignUpScreen}/>
-      <Stack.Screen name='Profile' component={ProfileScreen}/>
-    </Stack.Navigator>
-      
-
-      
-      {/* <StatusBar style="auto" /> */}
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}/>
+      <Tab.Screen name="Add" component={''} options={{
+          tabBarLabel: 'add',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="plus" color={color} size={size} />
+          ),
+        }}/>
+      <Tab.Screen name="Chat" component={ChatScreen} options={{
+          tabBarLabel: 'chat',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="chat" color={color} size={size} />
+          ),
+        }}/>
+      <Tab.Screen name="Profile" component={ProfileScreen} options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}/>
+         <Tab.Screen name='Sign In' component={SignInScreen}/>
+      {/* <Tab.Screen name='Sign Up' component={SignUpScreen}/> */}
+      </Tab.Navigator>
 
   </NavigationContainer>     
  
