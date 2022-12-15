e2e591304c817bda90d79d931b2a08cd1899a5f3
onyx <onyxsmtp@gmail.com>
2022-12-15 15:56:07 +0000

    Added Avatar options and image border to Add Items

--- src/components/Avatar.js
+++
@@ -1,4 +0,0 @@
-import React, { useState } from 'react';
-import Dialog from "react-native-dialog";
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
-import { SelectList } from 'react-native-dropdown-select-list'
